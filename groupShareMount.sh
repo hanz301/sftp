05154dc4dc40b4ae5bdb24658009b94b2dff736d
hanz301 <44664831+hanz301@users.noreply.github.com>
2018-11-13 10:39:52 +0100

    Update groupShareMount.sh

--- groupShareMount.sh
+++ groupShareMount.sh
@@ -1,7 +1,7 @@
 bindMount=$(mount --bind /home/sftp/groupshare/ /home/$users/groupShare/);
-
-
 chmod u=r,g=rwx,o= /home/sftp/groupshare/
+
+#Creates groupShare for all users in /home/, except if user is named sftp
 
 for users in $(ls /home)
 do 		
